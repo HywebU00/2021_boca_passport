398d877bbf6048c60ff9f32bafd75093ac1f042b
Lize Wu <retniw72@gmail.com>
2020-06-30 16:24:44 +0800

    2020-0630 上傳之前被 gitignore 隱藏的檔案

---
+++ config.rb
@@ -0,0 +1,33 @@
+require 'compass/import-once/activate' 
+# Require any additional compass plugins here. 
+
+# require 'breakpoint' 
+# require 'susy' 
+
+# Set this to the root of your project when deployed: 
+http_path = "/" 
+css_dir = "css" 
+sass_dir = "sass" 
+images_dir = "images" 
+javascripts_dir = "js" 
+
+# You can select your preferred output style here (can be overridden via the command line): 
+# output_style = :expanded or :nested or :compact or :compressed 
+output_style = :compressed 
+
+# To enable relative paths to assets via compass helper functions. Uncomment: 
+relative_assets = true 
+
+# To disable debugging comments that display the original location of your selectors. Uncomment: 
+line_comments = false 
+
+# If you prefer the indented syntax, you might want to regenerate this 
+# project again passing --syntax sass, or you can uncomment this: 
+# preferred_syntax = :sass 
+# and then run: 
+
+
+Encoding.default_external = "utf-8" 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+sourcemap = true 
