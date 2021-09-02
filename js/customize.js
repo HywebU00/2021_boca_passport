c7018034905fd4310cdf5c3d145ac19b1e5c0ccd
Lize Wu <retniw72@gmail.com>
2021-09-02 11:57:21 +0800

    2021-0902 新增icon、調整縮排對齊

--- js/customize.js
+++ js/customize.js
@@ -166,4 +166,23 @@ $(function() {
         focusOnSelect: true,
         infinite: true
     });
+
+    // password_toggle
+    var passShow = false;
+    $('.password_toggle').each(function(index, el) {
+        $(this).find('.btn-icon').off().click(function(e) {
+            if (!passShow) {
+                $(this).children('i').removeClass().addClass('i_show');
+                $(this).parents('.password_toggle').find('input[type="password"]').attr('type', 'text');
+                passShow = true;
+                // console.log(passShow);
+            } else {
+                $(this).children('i').removeClass().addClass('i_hide');
+                $(this).parents('.password_toggle').find('input[type="text"]').attr('type', 'password');
+                passShow = false;
+                // console.log(passShow);
+            }
+            e.preventDefault();
+        });
+    });
 });
