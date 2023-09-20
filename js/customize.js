aa708dd56e9a56ffc7bd64838209b70a37b0ac0e
Lize Wu <retniw72@gmail.com>
2023-09-20 17:09:40 +0800

    流程異動

--- js/customize.js
+++ js/customize.js
@@ -1,5 +1,32 @@
 // 自行加入的JS請寫在這裡
 $(function() {
+    // timer
+    $('.timer').siblings('.breadcrumb').addClass('act');
+
+    // 月份輪播
+    $('.multipleSlider').slick({
+        infinite: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 1099,
+            settings: {
+                dots: true,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },{
+            breakpoint: 765,
+            settings: {
+                dots: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+                    
+
     // 首頁輪播
     // $('.mpSlider').slick({
     //     mobileFirst: true,
@@ -194,26 +221,25 @@ $(function() {
     /////////////modal設定/////////////
     /*-----------------------------------*/
     $(function () {
-      $("#modal1").hide(); //先隱藏視窗
-      $(".modal").after('<div class="modal_overlay"></div>'); //新增透明底
-      $(".modal").prepend('<button type="button" class="close">關閉</button>'); //新增關閉按鈕
-      $(".modal_overlay").hide(); //隱藏透明底
-      //按鈕動作
-      $("#openModal").click(function (e) {
-        $(".modal_overlay").fadeIn(100);
-        $(".modal").fadeIn(100);
-        $("body").addClass("noscroll");
-        e.preventDefault();
-      });
-      //關閉function
-      function closeModal() {
-        $("#modal1").hide();
-        $(".modal_overlay").hide();
-        $("body").removeClass("noscroll");
-      }
-      //點選關閉按鈕及透明底都可關閉
-      $(".modal_overlay").click(closeModal);
-      $(".modal .close").click(closeModal);
+        $("#modal1").hide(); //先隱藏視窗
+        $(".modal").after('<div class="modal_overlay"></div>'); //新增透明底
+        $(".modal").prepend('<button type="button" class="close">關閉</button>'); //新增關閉按鈕
+        $(".modal_overlay").hide(); //隱藏透明底
+        //按鈕動作
+        $("#openModal").click(function (e) {
+            $(".modal_overlay").fadeIn(100);
+            $(".modal").fadeIn(100);
+            $("body").addClass("noscroll");
+            e.preventDefault();
+        });
+        //關閉function
+        function closeModal() {
+            $("#modal1").hide();
+            $(".modal_overlay").hide();
+            $("body").removeClass("noscroll");
+        }
+        //點選關閉按鈕及透明底都可關閉
+        // $(".modal_overlay").click(closeModal);
+        $(".modal .close").click(closeModal);
     });
-
 });
